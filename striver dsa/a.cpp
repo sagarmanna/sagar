013888f0e1e5fedb7988230156a74e6b18135289
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-12-27 23:20:16 +0530

    Add files via upload

---
+++ striver dsa/a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //2d array
+    int arr[3][5];
+    arr[1][3]=54;
+    cout<<arr[1][3];
+    return 0;
+
+}
---
+++ striver dsa/fun.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printhello(){
+   cout<<"Hello";
+}
+
+int main(){
+
+
+    return 0;
+  
+
+   
+    
+}
